03dc1f5c64ca0519eee6113236186d6ee9580a62
arifuddinkln <arifuddin@kln.gov.my>
2020-02-19 17:03:27 +0800

    selesai hari rabu

--- resources/views/item/list.blade.php
+++ resources/views/item/list.blade.php
@@ -8,15 +8,16 @@
                 <div class="card-header">Senarai Item</div>
 
                 <div class="card-body">
-
+					<a href="{{ route('item') }}" class="btn btn-primary">Tambah</a>
+                    <br><br>
                 	<table class="table table-bordered">
-                		<tr>
+                		<tr class="card-header">
                 			<td>No</td>
                 			<td>Nama</td>
                 			<td>Description</td>
                 			<td>Status</td>
 							<td>Harga</td>
-							<td>wujud</td>
+							<td>Tarikh Wujud</td>
                 			<td>Tindakan</td>
                 		</tr>
                 		<?php foreach ($items as $keyItem => $valueItem): ?>
@@ -24,13 +25,20 @@
                 			<td>{{ $keyItem+1 }}</td>
                 			<td>{{ $valueItem->name }}</td>
                 			<td>{{ $valueItem->description }}</td>
-                			<td>{{ $valueItem->status }}</td>
+                			<td>
+								<?php if($valueItem->status): ?>
+									Aktif
+								<?php else: ?>
+									Tidak Aktif
+								<?php endif ?>
+								
+							</td>
 							<td>{{ $valueItem->price }}</td>
-							<td>{{ $valueItem->created_at }}</td>
+							<td>{{ date('d/m/Y H:i:s', strtotime($valueItem->created_at)) }}</td>
                 			<td>
                 				<a class="btn btn-success btn-primary btn-xs" href="{{ route('item.form',$valueItem->item_id) }}" style="margin-bottom:10px">KEMASKINI</a>
                 				<br>
-                				<a class="btn btn-success btn-danger btn-xs" href="javascript:;">PADAM</a>
+                				<a class="btn btn-success btn-danger btn-xs" href="javascript:;" onclick="remove({{$valueItem->item_id}})">PADAM</a>
                 			</td>
                 		</tr>
                 		<?php endforeach ?>
@@ -43,3 +51,56 @@
 </div>
 
 @endsection
+
+@push('js')
+
+<script type="text/javascript">
+	function remove(id) {
+
+		swal({
+			title: "Padam Data",
+			text: "Data yang telah dipadam tidak boleh dikembalikan. Teruskan?",
+			icon: "warning",
+			buttons: ["Batal", { text: "Padam", closeModal: false }],
+			dangerMode: true,
+		})
+		.then((confirm) => {
+			if (confirm) {
+				$.ajax({
+					headers: {
+						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+					},
+					url: "{{ route('item') }}/form/"+id,
+					method: 'delete',
+					dataType: 'json',
+					async: true,
+					contentType: false,
+					processData: false,
+					success: function(data) {
+
+						swal({
+							title: data.title, 
+							text: data.message,
+							icon: data.status,
+							button: "OK",
+						})
+						.then((confirm)=> {
+							if(confirm){
+								location.reload();
+							}
+						});
+						//table.api().ajax.reload(null, false);
+						//location.reload();
+					}
+				});
+			}
+		});
+	
+	}
+	
+
+</script>
+
+@endpush
+
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,10 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('assets/plugins/sweetalert/sweetalert.min.js')}}" type="text/javascript"></script>
+
+    @stack('js')
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
