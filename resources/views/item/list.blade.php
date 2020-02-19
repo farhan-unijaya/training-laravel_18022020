17db0dbf94003cfd7ccf036d756051445faab56d
Mohd Farhan <farhan.unijaya@gmail.com>
2020-02-19 16:21:04 +0800

    letak status kalau 1 Aktif kalau bukan 1 X Aktif

--- resources/views/item/list.blade.php
+++ resources/views/item/list.blade.php
@@ -26,7 +26,13 @@
                 			<td>{{ $keyItem+1 }}</td>
                 			<td>{{ $valueItem->name }}</td>
                 			<td>{!! nl2br($valueItem->description) !!}</td>
-                			<td>{{ $valueItem->status }}</td>
+                			<td>
+                                <?php if ($valueItem->status): ?>
+                                    Aktif     
+                                <?php else: ?>
+                                    Tak Aktif
+                                <?php endif ?>         
+                            </td>
                 			<td>{{ $valueItem->price }}</td>
                 			<td>
                 				<a class="btn btn-success btn-primary btn-xs" href="{{ route('item.form',$valueItem->item_id) }}" style="margin-bottom:10px">KEMASKINI</a>
