7052154ee28fc7e6caec2bd74213c7fcb26d240c
suraya <surayajpn@gmail.com>
2020-02-19 17:02:49 +0800

    Selesai proses CRUD

--- resources/views/item/list.blade.php
+++ resources/views/item/list.blade.php
@@ -27,12 +27,19 @@
                 			<td>{{ $valueItem->name }}</td>
                             <!--<td>{{ $valueItem->description }}</td>-->
                 			<td>{!! nl2br($valueItem->description)!!}</td>
-                			<td>{{ $valueItem->status }}</td>
+                            <td>
+                                <?php if ($valueItem->status): ?>
+                                    Aktif
+                                <?php else: ?>
+                                    Tidak Aktif
+                                <?php endif ?>
+                            </td>
+                			<!--<td>{{ $valueItem->status }}</td>-->
                 			<td>{{ $valueItem->price }}</td>
                 			<td>
                 				<a class="btn btn-success btn-primary btn-xs" href="{{ route('item.form',$valueItem->item_id) }}" style="margin-bottom:10px">KEMASKINI</a>
                 				<br>
-                				<a class="btn btn-success btn-danger btn-xs" href="javascript:;">PADAM</a>
+                				<a class="btn btn-success btn-danger btn-xs" href="javascript:;" onclick="remove({{ $valueItem->item_id}})">PADAM</a>
                 			</td>
                 		</tr>
                 		<?php endforeach ?>
@@ -45,3 +52,57 @@
 </div>
 
 @endsection
+
+@push('js')
+
+<script type="text/javascript">
+
+function remove(id) {
+
+    swal({
+        title: "Padam Data",
+        text: "Data yang telah dipadam tidak boleh dikembalikan. Teruskan?",
+        icon: "warning",
+        buttons: ["Batal", { text: "Padam", closeModal: false }],
+        dangerMode: true,
+    })
+    .then((confirm) => {
+        if (confirm) {
+            $.ajax({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                url: "{{ route('item') }}/form/"+id,
+                method: 'delete',
+                dataType: 'json',
+                async: true,
+                contentType: false,
+                processData: false,
+                success: function(data) {
+
+                    swal({
+                        title: data.title, 
+                        text: data.message, 
+                        icon: data.status,
+                        button:"OK",
+                    })
+                    .then((confirm) => {
+                        if (confirm) {
+                            location.reload();
+                        }
+                    });                
+                }
+            });
+        }
+    });
+
+}
+
+
+
+
+
+
+</script>
+
+@endpush
