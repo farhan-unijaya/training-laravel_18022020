e3ebf55ca6ae96402575476dc7c8da21fd3e3076
jackobarzo <jackobarzo@gmail.com>
2020-02-19 11:55:10 +0800

    kemaskini welcome page. add senarai Item Link.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,6 +93,8 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                     <a href="{{ route('training') }}">Training Page</a>
+                    <a href="{{ route('item.list') }}">Senarai Item</a>
+
                 </div>
             </div>
         </div>
